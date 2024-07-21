42848317b9c8c9a7928a0f520460b1c6c6a2f21f
Vima001 <shanpuka58@gmail.com>
2024-07-21 14:29:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> VIMAMODS -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94776734030' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KRMjHCpT#ulqK1bP-ii3o5HVTiAMiVhU72rHIuGTBEdw_fT3Y7qc' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=uAlQhTDA#O-nVemy2o5-R-liRfmWf-IrrWR4D6WcGQxWUMr-WQrg' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
